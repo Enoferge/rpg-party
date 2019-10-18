e0ad42c9c12b94adeca0ff99e206dfa733bff580
Enoferge <jade.escapist@yandex.ru>
2019-10-18 21:28:42 +0300

    Updates

---
+++ precache-manifest.07d7948b18ae6ed027b1f2a2c46790a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84a4c55c8e8be62a6d4170dab0be92e",
+    "url": "/rpg-party/index.html"
+  },
+  {
+    "revision": "88ed3bd703e005e0b208",
+    "url": "/rpg-party/static/css/main.8e9ad639.chunk.css"
+  },
+  {
+    "revision": "aac8ecbb14c819fe65f8",
+    "url": "/rpg-party/static/js/2.eea72079.chunk.js"
+  },
+  {
+    "revision": "88ed3bd703e005e0b208",
+    "url": "/rpg-party/static/js/main.4b0afbb7.chunk.js"
+  },
+  {
+    "revision": "8b4a9b5d60d8f019c5e2",
+    "url": "/rpg-party/static/js/runtime-main.57f54270.js"
+  },
+  {
+    "revision": "8e1c5c9f3ef5b2bedd7081ae731711df",
+    "url": "/rpg-party/static/media/GirlA01.8e1c5c9f.png"
+  },
+  {
+    "revision": "0fe5c5a7d1c27ee5beaa3197b6c07db7",
+    "url": "/rpg-party/static/media/GirlB01.0fe5c5a7.png"
+  },
+  {
+    "revision": "f33bb42bd0444987e08e395241cab089",
+    "url": "/rpg-party/static/media/GirlC01.f33bb42b.png"
+  },
+  {
+    "revision": "ee24887124aaacfcf0428ec8deb498a9",
+    "url": "/rpg-party/static/media/GirlD01.ee248871.png"
+  },
+  {
+    "revision": "4b8a12507a9b3c74d260946ea476917a",
+    "url": "/rpg-party/static/media/Kelvinch.4b8a1250.otf"
+  },
+  {
+    "revision": "22b4a6e7cfcf775ac1e631c004e56512",
+    "url": "/rpg-party/static/media/MaleA01.22b4a6e7.png"
+  },
+  {
+    "revision": "1964f63399450ae3c4a75627f2b85fa2",
+    "url": "/rpg-party/static/media/MaleB01.1964f633.png"
+  },
+  {
+    "revision": "b0a7ad75ab3a3b00b759451c95ba4703",
+    "url": "/rpg-party/static/media/MaleC01.b0a7ad75.png"
+  },
+  {
+    "revision": "80920571050ba281b70fbdcfc15fa438",
+    "url": "/rpg-party/static/media/MaleD01.80920571.png"
+  },
+  {
+    "revision": "fb1527fe6fd21a28514bb67d3ca064d1",
+    "url": "/rpg-party/static/media/MaleE01.fb1527fe.png"
+  },
+  {
+    "revision": "5f5b45124692a24c1259778b1735474a",
+    "url": "/rpg-party/static/media/MaleF01.5f5b4512.png"
+  },
+  {
+    "revision": "c4a7579a1e4cf7d70b4e0d92abc7d91a",
+    "url": "/rpg-party/static/media/MaleG01.c4a7579a.png"
+  },
+  {
+    "revision": "eda1579de930ca04b817c5b939d1c56e",
+    "url": "/rpg-party/static/media/MaleH01.eda1579d.png"
+  },
+  {
+    "revision": "e99efff4ba2897ddf2be07db3e34bccf",
+    "url": "/rpg-party/static/media/bg-layout-1.e99efff4.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rpg-party/precache-manifest.07d7948b18ae6ed027b1f2a2c46790a8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rpg-party/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
